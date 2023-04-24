ba39ebaf7b5ecf4622c9ce763f9332d0b5baffa3
anightatheopera <ritalino7@hotmail.com>
2023-04-24 09:46:20 +0100

    html converter started

---
+++ Pug2Html/src/html_conv.py
@@ -0,0 +1,42 @@
+from parse import parse_pug
+from blocks import Tag, Literal
+from lexer import build_lexer
+
+
+def pug_to_html(pug_code):
+    # Build lexer
+    lexer = build_lexer()
+
+    # Tokenize Pug code
+    lexer.input(pug_code)
+    tokens = list(lexer)
+
+    # Parse Pug code
+    parsed = parse_pug(pug_code)
+
+    # Generate HTML code
+    def generate_html(node):
+        if isinstance(node, Tag):
+            attributes = " ".join(f'{k}="{v}"' for k, v in node.attrs.items())
+            start_tag = f"<{node.name} {attributes}>" if attributes else f"<{node.name}>"
+            children = "".join(generate_html(child) for child in node.children)
+            end_tag = f"</{node.name}>"
+            return f"{start_tag}{children}{end_tag}"
+        elif isinstance(node, Literal):
+            return node.value
+        else:
+            raise ValueError(f"Invalid node type: {type(node)}")
+
+    try:
+        return generate_html(parsed)
+    except Exception as e:
+        print(f"Error generating HTML: {e}")
+        print(f"Parsed tree: {parsed}")
+        raise
+
+pug_code = "div\ndiv"
+
+html_code = pug_to_html(pug_code)
+
+print(html_code)
+
